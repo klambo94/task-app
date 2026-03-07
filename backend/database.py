57aa1f2ed93352e2f5f196f89808085532a6a9c7
Riley <klambo94@gmail.com>
2026-03-07 18:08:39 -0500

    Added docker files including using postgres and adding Psycopg to the project

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,20 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DB_FILE_PATH = 'sqlite:///./task-app.db'
-engine = create_engine(DB_FILE_PATH, connect_args={"check_same_thread": False})
+
+POSTGRES_USER : str = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
+POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
+POSTGRES_DB : str = os.getenv("POSTGRES_DB","task-app")
+
+print(f"USER: {POSTGRES_USER}")
+print(f"PASSWORD: {POSTGRES_PASSWORD}")
+print(f"SERVER: {POSTGRES_SERVER}")
+print(f"DB: {POSTGRES_DB}")
+
+DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+engine = create_engine(url=DATABASE_URL)
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
