cb73be32f1e80aba278b1b31578c6d9810546a08
Riley <klambo94@gmail.com>
2026-03-07 18:12:57 -0500

    don't want those to print...

--- backend/database.py
+++ backend/database.py
@@ -10,11 +10,6 @@ POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
 POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
 POSTGRES_DB : str = os.getenv("POSTGRES_DB","task-app")
 
-print(f"USER: {POSTGRES_USER}")
-print(f"PASSWORD: {POSTGRES_PASSWORD}")
-print(f"SERVER: {POSTGRES_SERVER}")
-print(f"DB: {POSTGRES_DB}")
-
 DATABASE_URL = f"postgresql+psycopg2://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 engine = create_engine(url=DATABASE_URL)
 SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
