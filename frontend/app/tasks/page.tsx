0699dc374406c0bada04ce841f106db13fd9afee
Riley <klambo94@gmail.com>
2026-03-08 13:20:19 -0400

    fixing prod build

--- frontend/app/tasks/page.tsx
+++ frontend/app/tasks/page.tsx
@@ -1,6 +1,7 @@
 import {getTasks} from "@/app/lib/api";
 import TaskPageClient from "./TaskPageClient";
 import {TaskStatus} from "@/app/lib/types";
+export const dynamic = "force-dynamic"
 
 export default async function TaskPage() {
     const tasks = await getTasks()
